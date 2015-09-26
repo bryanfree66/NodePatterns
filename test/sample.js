fea3261e25f16ef17e58ab5ac668c4a5c2de6912
Bryan Freeman <bryanfree66@gmail.com>
2015-09-25 20:33:14 -0700

    initial commit

---
+++ test/sample.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+describe("Something", function(){
+
+    describe("The way it smells", function(){
+        it("is smelly", function(){
+            assert.ok(true);
+        });
+    });
+
+});
